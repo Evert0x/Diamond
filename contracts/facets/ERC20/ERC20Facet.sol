1a34bb8c073a20927742bc0ea7cddc2ffc22185e
Mick de Graaf <mick@mickdegraaf.nl>
2020-09-07 14:43:42 +0200

    Implemented ERC20 facet

---
+++ contracts/facets/ERC20/ERC20Facet.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.1;
+
+import "../../openzeppelin/token/ERC20/IERC20.sol";
+import "../../openzeppelin/math/SafeMath.sol";
+
+import "../../libraries/LibDiamondStorage.sol";
+
+import "./LibERC20Storage.sol";
+
+contract ERC20Facet is IERC20 {
+    using SafeMath for uint256;
+
+    function initialize(uint256 _initialSupply, string memory _name, string memory _symbol, uint8 _decimals) external {
+        LibDiamondStorage.DiamondStorage storage ds = LibDiamondStorage.diamondStorage();
+        LibERC20Storage.ERC20Storage storage es = LibERC20Storage.erc20Storage();
+
+        address currentOwner = ds.contractOwner;
+        require(msg.sender == currentOwner, "Must own the contract.");
+
+        _mint(msg.sender, _initialSupply);
+
+        es.name = _name;
+        es.symbol = _symbol;
+        es.decimals = _decimals;
+    }
+
+    function approve(address _spender, uint256 _amount) external override returns (bool) {
+        LibERC20Storage.erc20Storage().allowances[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function transfer(address _to, uint256 _amount) external override returns (bool) {
+        _transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) external override returns (bool) {
+        LibERC20Storage.ERC20Storage storage es = LibERC20Storage.erc20Storage();
+
+        // Update approval if not set to max uint256
+        if(es.allowances[_from][msg.sender] != uint256(-1)) {
+            uint256 newApproval = es.allowances[_from][msg.sender].sub(_amount);
+            es.allowances[_from][msg.sender] = newApproval;
+            emit Approval(_from, msg.sender, newApproval);
+        }
+
+        _transfer(_from, _to, _amount);
+    }
+
+    function allowance(address _owner, address _spender) external view override returns (uint256) {
+        return LibERC20Storage.erc20Storage().allowances[_owner][_spender];
+    }
+
+    function balanceOf(address _of) external view override returns (uint256) {
+        return LibERC20Storage.erc20Storage().balances[_of];
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        return LibERC20Storage.erc20Storage().totalSupply;
+    }
+
+    function _mint(address _to, uint256 _amount) internal {
+        LibERC20Storage.ERC20Storage storage es = LibERC20Storage.erc20Storage();
+
+        es.balances[_to] = es.balances[_to].add(_amount);
+        es.totalSupply = es.totalSupply.add(_amount);
+
+        emit Transfer(address(0), _to, _amount);
+    }
+
+    function _burn(address _from, uint256 _amount) internal {
+        LibERC20Storage.ERC20Storage storage es = LibERC20Storage.erc20Storage();
+
+        es.balances[_from] = es.balances[_from].sub(_amount);
+        es.totalSupply = es.totalSupply.sub(_amount);
+        emit Transfer(_from, address(0), _amount);
+    }
+
+    function _transfer(address _from, address _to, uint256 _amount) internal {
+        LibERC20Storage.ERC20Storage storage es = LibERC20Storage.erc20Storage();
+
+        es.balances[_from] = es.balances[_from].sub(_amount);
+        es.balances[_to] = es.balances[_to].add(_amount);
+
+        emit Transfer(_from, _to, _amount);
+    }
+
+}
