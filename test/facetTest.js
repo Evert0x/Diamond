22bcb9eca443d2ca10ecef00f1d00b4625d1ba83
Evert Kors <evert@hatching.io>
2020-09-17 19:14:48 +0200

    Rebase on lastest version

--- test/facetTest.js
+++ test/facetTest.js
@@ -2,7 +2,9 @@
 /* global contract artifacts web3 before it assert */
 
 const Diamond = artifacts.require('Diamond')
-const DiamondFacet = artifacts.require('DiamondFacet')
+const DiamondCutFacet = artifacts.require('DiamondCutFacet')
+const DiamondLoupeFacet = artifacts.require('DiamondLoupeFacet')
+const OwnershipFacet = artifacts.require('OwnershipFacet')
 const CallFacet = artifacts.require('CallFacet')
 const BasketFacet = artifacts.require('BasketFacet')
 const Comp = artifacts.require('Comp')
@@ -10,39 +12,47 @@ const GovernorAlpha = artifacts.require('GovernorAlpha')
 let zeroAddress = '0x0000000000000000000000000000000000000000'
 contract('FacetTest', async accounts => {
     let diamond;
-    let diamondFacet;
+    let diamondCutFacet
+    let diamondLoupeFacet
     let callFacet;
     let basketFacet;
     let addresses
 
     function getSelectors (contract) {
-        const selectors = contract.abi.reduce((acc, val) => {
-          if (val.type === 'function') {
-            return acc + val.signature.slice(2)
-          } else {
-            return acc
-          }
-        }, '')
-        return selectors
+      const selectors = contract.abi.reduce((acc, val) => {
+        if (val.type === 'function') {
+          acc.push(val.signature)
+          return acc
+        } else {
+          return acc
+        }
+      }, [])
+      return selectors
     }
 
     before(async () => {
         web3.eth.defaultAccount = accounts[0]
         diamond = await Diamond.deployed()
-        diamondFacet = new web3.eth.Contract(DiamondFacet.abi, diamond.address)
+        diamondCutFacet = new web3.eth.Contract(DiamondCutFacet.abi, diamond.address)
+        diamondLoupeFacet = new web3.eth.Contract(DiamondLoupeFacet.abi, diamond.address)
+        const ownershipFacet = new web3.eth.Contract(OwnershipFacet.abi, diamond.address)
         callFacet = await CallFacet.deployed()
         basketFacet = await BasketFacet.deployed()
-        addresses = await diamondFacet.methods.facetAddresses().call()
+        addresses = await diamondLoupeFacet.methods.facetAddresses().call()
 
         // Attach callFacet to diamond
         let selectors = getSelectors(callFacet)
         addresses.push(callFacet.address)
-        await diamondFacet.methods.diamondCut([callFacet.address + selectors], zeroAddress, '0x').send({ from: web3.eth.defaultAccount, gas: 1000000 })
+        await diamondCutFacet.methods.diamondCut(
+          [[callFacet.address, selectors]], zeroAddress, '0x'
+        ).send({ from: web3.eth.defaultAccount, gas: 1000000 })
 
         // Attach basketFacet to diamond
         selectors = getSelectors(basketFacet)
         addresses.push(basketFacet.address)
-        await diamondFacet.methods.diamondCut([basketFacet.address + selectors], zeroAddress, '0x').send({ from: web3.eth.defaultAccount, gas: 1000000 })
+        await diamondCutFacet.methods.diamondCut(
+          [[basketFacet.address, selectors]], zeroAddress, '0x'
+        ).send({ from: web3.eth.defaultAccount, gas: 1000000 })
 
         // Reinitialize both callFacet and basketFacet.
         // Using the diamond address
@@ -54,7 +64,7 @@ contract('FacetTest', async accounts => {
     //    see Comp.sol @ getPriorVotes
     //    see BasketFacet.sol @ joinPool
     // the compound contracts are heavily edited for testing purposes
-    it.only('Join comp pool and vote', async () => {
+    it('Join comp pool and vote', async () => {
         let ether = web3.utils.toWei("1", "ether");
         diamond = await Diamond.deployed()
         comp = await Comp.deployed()
